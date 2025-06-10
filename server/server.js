4b73b1759099c2b741ca39c8d0c4536697743776
Tushar1082 <134959329+Tushar1082@users.noreply.github.com>
2025-06-10 21:08:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ const { ObjectId } = require('mongodb');
 
 app.use(express.json()); //for parsing JSON
 app.use(express.urlencoded({ extended: true })); // for URL-encoded data
-app.use(cors({ origin: ['http://localhost:5173', 'https://spendingsmart.onrender.com', 'https://spendingsmart-bf0e0.web.app'] }));
+app.use(cors({ origin: ['https://expense-v2-1.onrender.com'] }));
 app.use(helmet());
 
 // Routes
